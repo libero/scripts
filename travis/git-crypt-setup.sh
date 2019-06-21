22808453e1072ef00a05dcb4adacbba280ee1bdc
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-21 13:29:40 +0100

    Check tools presence

--- travis/git-crypt-setup.sh
+++ travis/git-crypt-setup.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 set -e
 
-email="travis-ci-1234@example.com"
+
+if [ "$#" -ne 0 ]; then
+    echo "Add Travis CI to a git-crypt set up for a repository"
+	echo "Will create an uncommitted diff."
+    echo "Usage: $0"
+	exit 1
+fi
+
+email="travis-ci@example.com"
+
+if ! which travis; then
+	echo "Please install the \`travis\` cli"
+	exit 2
+fi
+
+if ! which gpg; then
+	echo "Please install \`gpg\`"
+	exit 3
+fi
+
+if ! which git-crypt; then
+	echo "Please install the \`git-crypt\` binary"
+	exit 4
+fi
 
 export GNUPGHOME="$(mktemp -d)"
 cat >foo <<EOF
@@ -17,10 +40,9 @@ cat >foo <<EOF
 EOF
 gpg --batch --gen-key foo
 
-#gpg --armor --export "${email}" > "${email}.pub"
-gpg --armor --export-secret-keys "${email}" > "${email}.key"
-git-crypt add-gpg-user "${email} --no-commit
+#gpg --armor --export "${email}" > "travis-ci.pub"
+gpg --armor --export-secret-keys "${email}" > travis-ci.key
+git crypt add-gpg-user "${email} --no-commit
 
-# TODO: check presence of tools at the start
-travis encrypt-file --com "${email}.key"
-rm ${email}.key
+travis encrypt-file --com travis-ci.key
+rm travis-ci.key
