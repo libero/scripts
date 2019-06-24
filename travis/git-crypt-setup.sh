c3e9de47b61a81f57c5db91969487e700c273a74
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-24 11:33:52 +0100

    Script for adding Travis CI to the git-crypt setup (#7)
    * Add git-crypt-setup.sh
    
    * Check tools presence
    
    * Add missing quotes
    
    * Remove key generation temporary script
    
    * Comb script with shellcheck
    
    * Move email variable close to usage
    
    * Remove public key debugging statement

---
+++ travis/git-crypt-setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+
+if [ "$#" -ne 0 ]; then
+    echo "Add Travis CI to a git-crypt set up for a repository"
+	echo "Will create an uncommitted diff."
+    echo "Usage: $0"
+	exit 1
+fi
+
+if ! travis -v; then
+	echo "Please install the \`travis\` cli"
+	exit 2
+fi
+
+if ! gpg --version; then
+	echo "Please install \`gpg\`"
+	exit 3
+fi
+
+if ! git-crypt version; then
+	echo "Please install the \`git-crypt\` binary"
+	exit 4
+fi
+
+email="travis-ci@example.com"
+GNUPGHOME="$(mktemp -d)"
+export GNUPGHOME
+cat >/tmp/key-generation.txt <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: RSA
+     Key-Length: 2048
+     Subkey-Length: 2048
+     Name-Real: Travis CI
+     Name-Email: ${email}
+     Expire-Date: 0
+     %commit
+     %echo done
+EOF
+gpg --batch --gen-key /tmp/key-generation.txt
+
+gpg --armor --export-secret-keys "${email}" > travis-ci.key
+git crypt add-gpg-user --no-commit "${email}"
+
+travis encrypt-file --com travis-ci.key
+rm travis-ci.key
+rm /tmp/key-generation.txt
